c8959035cafcf27bc67c632fb398c619819efe24
Changyeong <ocy1011@naver.com>
2020-01-04 00:22:27 +0900

    baekjoon 2588

---
+++ baekjoon/2588/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int A, B;
+  cin >> A >> B;
+  cout << A * (B % 10) << "\n"
+       << A * ((B / 10) % 10) << "\n"
+       << A * (B / 100) << "\n"
+       << A * B << "\n";
+  return 0;
+}
