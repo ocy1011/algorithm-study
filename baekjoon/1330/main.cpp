2b603d59181eda1390a54d672f30460dff5d696f
Changyeong <ocy1011@naver.com>
2020-01-04 00:32:20 +0900

    baekjoon 1330

---
+++ baekjoon/1330/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int A, B;
+  cin >> A >> B;
+  if (A > B)
+    cout << ">"
+         << "\n";
+  else if (A < B)
+    cout << "<"
+         << "\n";
+  else
+    cout << "=="
+         << "\n";
+  return 0;
+}
